694c0cea363d31eb05659e9ac7b9f6d6f8a6d9d3
FieryXJoe <josephmsherry@comcast.net>
2022-04-19 14:18:41 -0400

    Copied Box from inclass exercise

---
+++ component-lib/src/Box.js
@@ -0,0 +1,19 @@
+/**
+ * Copied from homework, if it ain't broke don't fix it
+ */
+const defaultStyle = { fontSize: 24, width: 200, height: 100 };
+const Box = (props) => {
+    return (<div className="box" style={{ ...defaultStyle, ...props.style, width: props.size == "small" ? 200 : props.size == "medium" ? 400 : 800, borderRadius: props.roundedCorners ? 25 : 0 }}>
+        {props.children}
+    </div>);
+};
+Box.propTypes = {
+    style: PropTypes.object,
+    children: PropTypes.node.isRequired,
+    size: PropTypes.string.isRequired,
+    roundedCorners: PropTypes.bool
+};
+Box.defaultProps = {
+    size: "small",
+    roundedCorners: false
+};
