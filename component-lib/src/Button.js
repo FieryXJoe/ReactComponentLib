ad64cc3f66030728c2bc1755bb3c85bf3ffa8979
FieryXJoe <josephmsherry@comcast.net>
2022-04-19 14:41:36 -0400

    First pass at Button.js

---
+++ component-lib/src/Button.js
@@ -0,0 +1,22 @@
+const defaultStyle = {height: 100 };
+const Box = (props) => {
+    //Will use same Size for Font * width for now
+    return (<div className="box" style={{ ...defaultStyle, ...props.style, width: props.size == "small" ? 200 : props.size == "medium" ? 400 : 800, borderRadius: props.roundedCorners ? 25 : 0, 
+    fontSize: props.size == "small" ? 24 : props.size == "medium" ? 36 : 48, color: props.color, backgroundColor: props.BGColor}} onClick={props.onClick}>
+        {props.children}
+    </div>);
+};
+Box.propTypes = {
+    style: PropTypes.object,
+    children: PropTypes.node.isRequired,
+    size: PropTypes.string.isRequired,
+    roundedCorners: PropTypes.bool,
+    onClick: PropTypes.func.isrequired,
+    BGColor: PropTypes.string.isrequired,
+    color: PropTypes.string
+};
+Box.defaultProps = {
+    size: "small",
+    roundedCorners: false,
+    color: "black"
+};
